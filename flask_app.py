ae2e981dcd19a3612f0948808de786caa84dcfa3
GonzaloChavasse <gonzalochavasse@gmail.com>
2026-02-21 17:25:59 -0300

    feat: Add no-cache headers to force browser to load latest version on changes

--- flask_app.py
+++ flask_app.py
@@ -844,4 +844,13 @@ if __name__ == '__main__':
             db.session.commit()
         except:
             pass  # Column already exists
+
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+if __name__ == '__main__':
     app.run(debug=True)
